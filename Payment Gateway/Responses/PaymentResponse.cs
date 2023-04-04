42f01f53133c888321a7bdd1bbe2ead49d2be8a2
igor-couto <igor.fcouto@gmail.com>
2023-04-04 14:43:02 -0300

    feat: add JsonPropertyName in create payment return

--- Payment Gateway/Responses/PaymentResponse.cs
+++ Payment Gateway/Responses/PaymentResponse.cs
@@ -1,17 +1,36 @@
-﻿using Domain.Entities;
+﻿using System.Text.Json.Serialization;
+using Domain.Entities;
 
 namespace PaymentGatewayAPI.Responses;
 
 public class PaymentResponse
 {
+
+    [JsonPropertyName("id")]
     public Guid Id { get; init; }
+    
+    [JsonPropertyName("checkoutId")]
     public Guid CheckoutId { get; init; }
+    
+    [JsonPropertyName("shopperId")]
     public Guid ShopperId { get; init; }
+    
+    [JsonPropertyName("merchantId")]
     public Guid MerchantId { get; init; }
+    
+    [JsonPropertyName("amount")]
     public string Amount { get; init; }
+
+    [JsonPropertyName("currency")]
     public string Currency { get; init; }
+    
+    [JsonPropertyName("maskedCreditCardNumber")]
     public string MaskedCreditCardNumber { get; init; }
+    
+    [JsonPropertyName("paymentStatus")]
     public string PaymentStatus { get; init; }
+
+    [JsonPropertyName("statusMessage")]
     public string? StatusMessage { get; init; }
 
     public PaymentResponse(Payment payment)

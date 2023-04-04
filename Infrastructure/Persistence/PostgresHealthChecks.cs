e1b99983bdeef4782ff565dde9d08dd6d31d02b0
igor-couto <igor.fcouto@gmail.com>
2023-04-04 00:58:13 -0300

    feat: add health check

---
+++ Infrastructure/Persistence/PostgresHealthChecks.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+
+namespace Infrastructure.Persistence;
+
+public class PostgresHealthChecks : IHealthCheck
+{
+    private readonly string _connectionString;
+    private const string _healthQuery = "SELECT 1;";
+    private readonly ILogger<PostgresHealthChecks> _logger;
+
+    public PostgresHealthChecks(IConfiguration configuration, ILogger<PostgresHealthChecks> logger)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+        _logger = logger;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            using var connection = new NpgsqlConnection(_connectionString);
+            await connection.OpenAsync(cancellationToken);
+
+            using var command = connection.CreateCommand();
+            command.CommandText = _healthQuery;
+
+            await command.ExecuteScalarAsync(cancellationToken);
+
+            return new HealthCheckResult(HealthStatus.Healthy, "PostgreSQL database is working properly.");
+        }
+        catch (Exception exception)
+        {
+            const string errorMessage = "Failed to connect to PostgreSQL database";
+
+            _logger.LogError(exception, errorMessage);
+
+            return new HealthCheckResult(context.Registration.FailureStatus, errorMessage);
+        }
+    }
+}
--- Payment Gateway/Configuration/HealthCheckConfiguration.cs
+++ Payment Gateway/Configuration/HealthCheckConfiguration.cs
@@ -1,10 +1,59 @@
-﻿namespace PaymentGatewayAPI.Configuration;
+﻿using System.Text.Json;
+using System.Text;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Infrastructure.Persistence;
+
+namespace PaymentGatewayAPI.Configuration;
 
 public static class HealthCheckConfiguration
 {
     public static void AddHealthCheck(this IServiceCollection services)
-        => services.AddHealthChecks();
+    {
+        services
+            .AddHealthChecks()
+            .AddCheck<PostgresHealthChecks>("database", HealthStatus.Unhealthy, timeout: TimeSpan.FromSeconds(4));
+    }
 
     public static void UseHealthCheckConfiguration(this WebApplication app)
-        => app.MapHealthChecks("/health");
+    {
+        app.UseHealthChecks("/health", new HealthCheckOptions { Predicate = _ => true, ResponseWriter = WriteResponse });
+    }
+
+    private static Task WriteResponse(HttpContext context, HealthReport healthReport)
+    {
+        context.Response.ContentType = "application/json; charset=utf-8";
+
+        var options = new JsonWriterOptions { Indented = true };
+
+        using var memoryStream = new MemoryStream();
+        using (var jsonWriter = new Utf8JsonWriter(memoryStream, options))
+        {
+            jsonWriter.WriteStartObject();
+            jsonWriter.WriteString("status", healthReport.Status.ToString());
+            jsonWriter.WriteStartObject("results");
+
+            foreach (var healthReportEntry in healthReport.Entries)
+            {
+                jsonWriter.WriteStartObject(healthReportEntry.Key);
+                jsonWriter.WriteString("status", healthReportEntry.Value.Status.ToString());
+                jsonWriter.WriteString("description", healthReportEntry.Value.Description);
+
+                foreach (var item in healthReportEntry.Value.Data)
+                {
+                    jsonWriter.WritePropertyName(item.Key);
+
+                    JsonSerializer.Serialize(jsonWriter, item.Value,
+                        item.Value?.GetType() ?? typeof(object));
+                }
+
+                jsonWriter.WriteEndObject();
+                jsonWriter.WriteEndObject();
+            }
+
+            jsonWriter.WriteEndObject();
+            jsonWriter.WriteEndObject();
+        }
+        return context.Response.WriteAsync(Encoding.UTF8.GetString(memoryStream.ToArray()));
+    }
 }
